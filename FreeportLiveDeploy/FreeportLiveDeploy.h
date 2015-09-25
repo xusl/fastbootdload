a431015dd5acbe216a88c0ffb2a1ab8d86b4ce24
Xu shenlong <shenlong.xu.sz@tcl.com>
2015-09-25 09:02:49 +0800

    add Project FreeportLiveDeploy

---
+++ FreeportLiveDeploy/FreeportLiveDeploy.h
@@ -0,0 +1,32 @@
+
+// FreeportLiveDeploy.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CFreeportLiveDeployApp:
+// See FreeportLiveDeploy.cpp for the implementation of this class
+//
+
+class CFreeportLiveDeployApp : public CWinApp
+{
+public:
+	CFreeportLiveDeployApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CFreeportLiveDeployApp theApp;
---
+++ FreeportLiveDeploy/FreeportLiveDeployDlg.cpp
@@ -0,0 +1,247 @@
+
+// FreeportLiveDeployDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "FreeportLiveDeploy.h"
+#include "FreeportLiveDeployDlg.h"
+#include "afxdialogex.h"
+#include "log.h"
+#include "adbhost.h"
+#include <string.h>
+#include <algorithm>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CFreeportLiveDeployDlg dialog
+
+
+
+
+CFreeportLiveDeployDlg::CFreeportLiveDeployDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CFreeportLiveDeployDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CFreeportLiveDeployDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CFreeportLiveDeployDlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_DEVICECHANGE()
+	ON_WM_TIMER()
+	ON_MESSAGE(UI_MESSAGE_INIT_DEVICE, &CFreeportLiveDeployDlg::OnInitDevice)
+END_MESSAGE_MAP()
+
+
+// CFreeportLiveDeployDlg message handlers
+
+BOOL CFreeportLiveDeployDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	// TODO: Add extra initialization here
+  StartLogging(L"FreeportLiveDeploy.log", "all", "all");
+  m_bSwitchDisk = FALSE;
+
+  m_hDevchangeTips = (CStatic *)GetDlgItem(IDC_STATIC_DEVCHANGE_TIPS);
+  RegisterAdbDeviceNotification(this->m_hWnd);
+  adb_usb_init();
+
+  if (kill_adb_server(DEFAULT_ADB_PORT) == 0) {
+    SetTimer(TIMER_PROFILE_LIST, 1000, NULL);
+  } else {
+    GetProfilesList(TRUE);
+  }
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CFreeportLiveDeployDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CFreeportLiveDeployDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+usb_handle* CFreeportLiveDeployDlg::GetUsbHandle() {
+  usb_handle* handle;
+  find_devices(true);
+  for (handle = usb_handle_enum_init();
+       handle != NULL ;
+       handle = usb_handle_next(handle)) {
+    break;
+  }
+  return handle;
+}
+VOID CFreeportLiveDeployDlg::GetProfilesList(BOOL trySwitchDisk) {
+  m_hUSBHandle = GetUsbHandle();
+  if (m_hUSBHandle != NULL) {
+  adbhost adb(m_hUSBHandle, usb_port_address(m_hUSBHandle));
+       // conf_file_char = WideStrToMultiStr(conf_file);
+      
+            
+
+      //adb.sync_push("data\\fatlabel", "/usr/sbin/fatlabel");
+      adb.sync_push("data\\formatsdcard.sh",  "/usr/oem/formatsdcard.sh");
+      adb.sync_push("data\\umount.sh", "/usr/oem/umount.sh");
+      adb.sync_push("data\\restartusb.sh", "/usr/oem/restartusb.sh");
+
+  //CheckDeviceProfilePath(m_hUSBHandle);
+  //DoGetProfilesList(m_hUSBHandle);
+  } else if (trySwitchDisk) {
+  PostMessage(UI_MESSAGE_INIT_DEVICE, (WPARAM)0, (LPARAM)NULL);
+  }
+}
+BOOL CFreeportLiveDeployDlg::OnDeviceChange(UINT nEventType, DWORD_PTR dwData)
+{
+  if (dwData == 0)
+  {
+    WARN("OnDeviceChange, dwData == 0 .EventType: 0x%x", nEventType);
+    return FALSE;
+  }
+
+  DEV_BROADCAST_HDR* phdr = (DEV_BROADCAST_HDR*)dwData;
+  PDEV_BROADCAST_DEVICEINTERFACE pDevInf = (PDEV_BROADCAST_DEVICEINTERFACE)phdr;
+
+  DEBUG("OnDeviceChange, EventType: 0x%x, DeviceType 0x%x",
+        nEventType, phdr->dbch_devicetype);
+
+  if (nEventType == DBT_DEVICEARRIVAL) {
+    switch( phdr->dbch_devicetype ) {
+    case DBT_DEVTYP_DEVNODE:
+      WARN("OnDeviceChange, get DBT_DEVTYP_DEVNODE");
+      break;
+    case DBT_DEVTYP_VOLUME:
+      {
+        /* enumerate devices and shiftdevice
+        */
+        PDEV_BROADCAST_DEVICEINTERFACE pDevInf =
+               (PDEV_BROADCAST_DEVICEINTERFACE)phdr;
+        //    UpdateDevice(pDevInf, dwData);
+        WARN("OnDeviceChange, get DBT_DEVTYP_VOLUME");
+        //we handle this event when app is launched,
+        //if device plug in before app start, we receive this, because we switch device,
+        // and volumn (disk device) will enumerate with adb interface.
+        // We do this, for if device is release build, it run without adb, so we need to switch.
+        // test 1: launch app, plug in device, to see whether switch device.
+        // test 2: run adb server, plugin device , luanch app.
+        // test 3: run adb server, launch app, plugin device (if app launcher, adb can not start server).
+        // test 3 is not necessary, because user can not very quick to pulgin device after launcher the app.
+        if (m_bSwitchDisk == FALSE && m_hUSBHandle == NULL) {
+          DEBUG("SET TIMER_SWITCH_DISK");
+		  m_hDevchangeTips->SetWindowText(_T("检测到设备，请稍候，正在切换设备当中..."));
+          SetTimer(TIMER_SWITCH_DISK, 5000, NULL);
+        }
+        break;
+      }
+    case DBT_DEVTYP_DEVICEINTERFACE:
+      {
+        if (m_bSwitchDisk == FALSE) {
+          DEBUG("KILL TIMER_SWITCH_DISK");
+          KillTimer(TIMER_SWITCH_DISK);
+        } else {
+          m_bSwitchDisk = FALSE;
+		  m_hDevchangeTips->SetWindowText(_T(""));
+        }
+        GetProfilesList(FALSE);
+      }
+      break;
+    }
+  } else if (nEventType == DBT_DEVICEREMOVECOMPLETE) {
+    if (phdr->dbch_devicetype == DBT_DEVTYP_DEVICEINTERFACE) {
+
+      ASSERT(lstrlen(pDevInf->dbcc_name) > 4);
+      if (m_hUSBHandle != NULL) {
+        usb_close(m_hUSBHandle);
+        m_hUSBHandle = NULL;
+      }
+
+	  m_hDevchangeTips->SetWindowText(_T("设备已被拔出！"));
+    }
+  }
+
+  return TRUE;
+}
+void CFreeportLiveDeployDlg::OnTimer(UINT_PTR nIDEvent)
+{
+  CDialog::OnTimer(nIDEvent);
+  if (nIDEvent == TIMER_PROFILE_LIST) {
+    DEBUG("HANDLE TIMER_PROFILE_LIST TIMER");
+    GetProfilesList(TRUE);
+  } else if (nIDEvent == TIMER_SWITCH_DISK){
+    DEBUG("HANDLE TIMER_SWITCH_DISK TIMER");
+    PostMessage(UI_MESSAGE_INIT_DEVICE, (WPARAM)0, (LPARAM)NULL);
+  }
+
+  KillTimer(nIDEvent);
+}
+LRESULT  CFreeportLiveDeployDlg::OnInitDevice(WPARAM wParam, LPARAM lParam) {
+  std::vector<CString> devicePath;
+  GetDeviceByGUID(devicePath, &GUID_DEVINTERFACE_DISK);
+  int cdromSize = devicePath.size();
+
+  for(int i = 0; i < cdromSize; i++)
+  {
+    CString path = devicePath[i];
+    if (path.Find(_T("\\\\?\\usbstor#")) == -1) {
+      LOG("Fix DISK %S:", path);
+    } else {
+      path.MakeUpper();
+      if (path.Find(_T("ONETOUCH")) == -1 && path.Find(_T("ALCATEL")) == -1)
+        LOG("USB Stor %S is not alcatel",path);
+      else {
+        CSCSICmd scsi = CSCSICmd();
+        LOG("do switch device %S", devicePath[i]);
+        scsi.SwitchToDebugDevice(devicePath[i]);
+        m_bSwitchDisk = TRUE;
+        //scsi.SwitchToDebugDevice(_T("\\\\?\\H:"));
+        break;
+      }
+    }
+  }
+  devicePath.clear();
+
+  return 0;
+}
